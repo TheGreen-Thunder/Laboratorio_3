09988939a38b792d995d9b86b5934dfae206ff1e
Simón Molina <smolina2025@alu.uct.cl>
2025-11-07 23:54:14 -0300

    add lista
    se agrego los ejercicios de la lista de compresion en un solo py

---
+++ Lista_por_compresion.py
@@ -0,0 +1,9 @@
+numeros = [i for i in range(1, 51)]
+cuadrados = [i*i for i in range(1, 21) if i % 2 == 0]
+letras = [p[0] for p in ["python","java","c++","ruby"]]
+divisibles = [i for i in range(1, 31) if i % 3 == 0]
+
+print(numeros)
+print(cuadrados)
+print(letras)
+print(divisibles)
