034f2aefdef97451b9aec3b2a32e3e6f024bd764
Simón Molina <smolina2025@alu.uct.cl>
2025-11-07 23:55:23 -0300

    add lambda
    se agrego los ejercicios de lambda en un solo py

---
+++ lambda.py
@@ -0,0 +1,9 @@
+mayor = lambda a, b: a if a > b else b
+longitud = lambda cadena: len(cadena)
+primer_elemento = lambda lista: lista[0]
+doble = lambda n: n * 2
+
+print(mayor(10, 3))
+print(longitud("python"))
+print(primer_elemento([5, 9, 2]))
+print(doble(8))       
